60030018b60757bd4a760aa3265254f0b5996ac1
dosikoushal <46111030+dosikoushal@users.noreply.github.com>
2020-10-06 19:22:28 +0530

    Update question.js

--- src/client/modules/ui/question/question.js
+++ src/client/modules/ui/question/question.js
@@ -1,12 +1,19 @@
 import { LightningElement, api } from 'lwc';
 
+const TIMER_INTERVAL_MS = 20;
+
 export default class Question extends LightningElement {
     @api question;
     isSaving;
+    progress = 0;
+    progressBarStyle = '';
+    timerId;
 
+    /* Original code
     connectedCallback() {
         this.isSaving = false;
     }
+    */
 
     handleAnswerClick(event) {
         // Prevent duplicate answers
@@ -22,5 +29,32 @@ export default class Question extends LightningElement {
             }
         });
         this.dispatchEvent(answerEvent);
+    }
+
+    connectedCallback() {
+        const durationMs = this.duration * 1000;
+        let elapsedMs = 0;
+        // eslint-disable-next-line @lwc/lwc/no-async-operation
+        this.timerId = setInterval(() => {
+            elapsedMs += TIMER_INTERVAL_MS;
+            const progressPercent = elapsedMs / durationMs;
+            this.progress = Math.floor(100 * progressPercent);
+            const color = this.getColor(progressPercent);
+            this.progressBarStyle = `width: ${this.progress}%; background-color: ${color};`;
+            if (this.progress >= 100) {
+                this.progress = 100;
+                clearInterval(this.timerId);
+                this.dispatchEvent(new CustomEvent('timeout'));
+            }
+        }, TIMER_INTERVAL_MS);
+    }
+
+    getColor(percent) {
+        const hue = ((1 - percent) * 120).toString(10);
+        return 'hsl(${hue}, 100%, 50%)';
+    }
+
+    disconnectedCallback() {
+        clearInterval(this.timerId);
     }
 }
